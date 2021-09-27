ac080f7068a1f6c5f08116aadb3589a2d9d281c6
sergP80 <svpuzyrev@gmail.com>
2021-09-27 17:18:41 +0300

    Add files via upload
    Added mq3 example via esp8266

---
+++ ESP8266_Blynk_MQ3/ESP8266_Blynk_MQ3.ino
@@ -0,0 +1,171 @@
+/*
+ Link to boards https://dl.espressif.com/dl/package_esp32_index.json, http://arduino.esp8266.com/stable/package_esp8266com_index.json
+ Dependencies:
+  Blynk,
+  Dht sensor library
+  Adafruit Common Sens Lab
+  NOTE! Don't forget to choose ESPxx board
+*/
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <WiFiManager.h>
+
+#define MQ3_PIN A0
+#define LED_RED_PIN 5
+
+float baseLine = 0.0f;
+
+int prevButtonPressTimeStamp = 0;
+const int buttonPressInterval = 100;
+
+BlynkTimer timer;
+const char api_key[] = "kZF7vOlCq14Ypj_2WrfzbZXIOsI-1K0v";
+const char* wifi_ap_name = "TempHumMonitorAP";
+const char* wifi_ap_key = "tpcs2020";
+
+void setup()
+{
+    Serial.begin(115200);
+    
+    if (WiFi.SSID() == "")
+    {
+      Serial.println("No saved connections");
+      initConnection(); 
+    }
+    else
+    {
+      Serial.println("Try to connect to last network...");
+      WiFi.begin(WiFi.SSID().c_str(), WiFi.psk().c_str());
+      
+      delay(3000);
+      
+      if (WiFi.waitForConnectResult() != WL_CONNECTED)
+      {
+        initConnection();
+      }    
+    }
+
+    Blynk.begin(api_key, WiFi.SSID().c_str(), WiFi.psk().c_str());
+    
+    Serial.print("Connected to ");
+    Serial.println(WiFi.SSID());
+    
+    pinMode(MQ3_PIN, INPUT);    
+    calibrateMQ3(100);
+
+    pinMode(LED_RED_PIN, OUTPUT);    
+    digitalWrite(LED_RED_PIN, LOW);
+    timer.setInterval(1000L, sendSensorData);
+}
+
+void loop()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("=======================");
+    Serial.println("Not connected. Try to reconnect...");
+    Serial.println("=======================");
+    ESP.reset();
+  } else 
+  {
+    Blynk.run();
+    timer.run();
+  }
+}
+
+void initConnection()
+{
+  WiFiManager wifiManager;
+  wifiManager.resetSettings();
+  wifiManager.autoConnect(wifi_ap_name, wifi_ap_key);
+}
+
+void sendSensorData()
+{
+  float BAC = getMQ3Data();
+  if (isnan(BAC))
+  {
+    Serial.println("Failed to read from MQ3 sensor!");
+    return;
+  }
+  Serial.print("Current C2H5OH level: ");
+  Serial.print(BAC);
+  Serial.println("%%");
+  Blynk.virtualWrite(V7, BAC);
+}
+
+float getMQ3Data()
+{
+  int count = 10;
+  float sensorValue = 0.0f;
+  for (int i = 0; i < count; ++i)
+  {
+    sensorValue += analogRead(MQ3_PIN);
+    delay(10);
+  }
+  
+  sensorValue /= count;
+  sensorValue -= baseLine;
+  
+  float outputValue = map(sensorValue, 0, 1023.0, 0, 255.0);
+  float outputVoltage = outputValue*(5.0/255.0);
+  float BAC = 0.0;
+  if (outputVoltage > 0.10)
+  {
+    BAC = -0.6694f*((outputVoltage)*(outputVoltage)) + (0.5634f*outputVoltage) + 0.0451f;
+    if (BAC>=0.0f)
+    {
+      BAC=BAC;
+    }
+    else
+    {
+      BAC=0.00f; 
+    }
+  }
+  else 
+    if (-0.06f <= outputVoltage &&  outputVoltage < 0.04f)
+    {
+      BAC = 0.02f;
+    }
+    else
+      if (0.04f <= outputVoltage && outputVoltage <= 0.10f)
+      {
+        BAC = 0.08f;
+      }
+  return BAC;    
+}
+
+void calibrateMQ3(int samples)
+{
+  float result;
+  for (int i = 0; i < samples; ++i)
+  {
+    float value = analogRead(MQ3_PIN);
+    result = i ? (result + value)/2 : value;
+    delay(100);
+  }
+
+   baseLine = result + 20;
+}
+
+BLYNK_WRITE(V8) {
+  if (param.asInt())
+  {
+    prevButtonPressTimeStamp = millis();
+  }
+  else
+  {
+    if (millis() - prevButtonPressTimeStamp > buttonPressInterval)
+    {
+      Serial.println("Reset device");
+      ESP.reset();
+    }
+  }
+}
+
+BLYNK_WRITE(V9) {
+  Serial.println("Signal led");
+  digitalWrite(LED_RED_PIN, param.asInt());
+}
